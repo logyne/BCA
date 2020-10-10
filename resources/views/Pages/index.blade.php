56dcf8e5b751be93e1de1c983779d6bc0b9ab263
Logyn Medhat <logynmedhat@Logyns-MacBook-Air.local>
2020-10-10 19:37:13 +0200

    first commit

---
+++ resources/views/Pages/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','laravel')}}</title>
+
+        
+            
+       
+    </head>
+    <body>
+       <h1>WELCOME to laravel</h1>;
+        <p>LAAAAARAVEl</p>;
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/' ,'PagesController@index');
+//Route::get('/about','PagesController@services');
+//Route::get('/service' ,'PagesController@about');
+/*
+Route::get('/users/{id}', function ($id) {
+    return 'This is user '.$id;
+});
+Route::get('/users/{id}/{user}', function ($id,$user) {
+    return 'This is the  user '.$id.'and his id'.$user;
+});*/
+Route::get('/', function () {
+    return '<h1>Hello World!</h1>';
+});
+
+/*Route::get('/about', function () {
+    return view('Pages.about');
+});*///
+//Route::get('/' ,'Pagecontroller@index');
+
